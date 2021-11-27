39e4cef00fa630f455faec24f7b0b0aa3634db33
Marco Castro <marcodotcastro@gmail.com>
2021-11-27 08:17:29 -0300

    feat: publish subscription

--- app/jobs/subscribe_job.rb
+++ app/jobs/subscribe_job.rb
@@ -2,7 +2,6 @@ class SubscribeJob < ApplicationJob
   queue_as :default
 
   def perform(params)
-    subscription = Subscription.create(description: params["turma"]["nome"])
-    puts "Subscription #{subscription.id} created!"
+    QueueService.new({queue_name: "desenvolvendome.subscribe"}).publish(params)
   end
 end
