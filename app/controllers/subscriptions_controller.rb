bcb2f530513e40b3e74853927fe90d22ca081ee0
Marco Castro <marcodotcastro@gmail.com>
2021-11-27 06:49:56 -0300

    feat(subscription): write

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -5,4 +5,15 @@ class SubscriptionsController < ApplicationController
     render json: { message: "#{Subscription.count} inscrições na turma SPDv1.6 realizadas com sucesso!" }
   end
 
+  def write
+    Subscription.create(description: params["turma"]["nome"])
+
+    render json: { message: "#{subscription_params["aluno"]}, sua inscrição na turma #{params["turma"]["nome"]} foi realizada com sucesso!" }
+  end
+
+  private
+
+  def subscription_params
+    params.permit(:aluno, turma: [:nome])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   get 'subscriptions/read'
+  post 'subscriptions/write'
 end
